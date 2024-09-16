5e2726145dddba5ad29f84268721d91e8a7e2843
Sonya Castro <sonya.castro.gomez@gmail.com>
2024-09-16 00:28:21 -0500

    fix(Footer): fix footer accessibility

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,28 +1,28 @@
-import LinkedinIcon from '@/assets/icons/linkedin.svg';
-import TwitterIcon from '@/assets/icons/twitter.svg';
-import GithubIcon from '@/assets/icons/github.svg';
+import LinkedinIcon from "@/assets/icons/linkedin.svg";
+import TwitterIcon from "@/assets/icons/twitter.svg";
+import GithubIcon from "@/assets/icons/github.svg";
 
 /**
- * This is a Footer component. 
+ * This is a Footer component.
  */
 export const Footer = () => {
   return (
-    <div className='p-10 flex flex-col gap-5 border-t border-t-slate-300'>
-      <div className="flex gap-5 justify-center w-full ">
+    <div className="flex flex-col gap-5 border-t border-t-slate-300 p-10">
+      <div className="flex w-full justify-center gap-5">
         <a href="https://www.linkedin.com/in/sonya-castro/" target="_blank">
-          <img src={LinkedinIcon} alt="linkedin icon" width={30}/>
+          <img src={LinkedinIcon} alt="linkedin icon" width={30} />
         </a>
-        
+
         <a href="https://x.com/sonyac_dev" target="_blank">
           <img src={TwitterIcon} alt="twitter icon" width={30} />
         </a>
-        
+
         <a href="https://github.com/Sonya-c" target="_blank">
           <img src={GithubIcon} alt="github icon" width={30} />
         </a>
       </div>
       <div>
-        <p className="text-center text-sm font-semibold text-gray-400">
+        <p className="text-center text-sm">
           © Sonya Castro 2021 All rights reserved.
         </p>
       </div>
