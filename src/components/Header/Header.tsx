c3461ba27a6b6c4d46b98324ae2bb3cd02cfcba6
Sonya Castro <sonya.castro.gomez@gmail.com>
2024-09-15 23:29:03 -0500

    smoth scrolling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,9 +8,21 @@ import { useState } from 'react';
 export const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   
+  const scrollTo = (id: string) => {
+    const element = document.getElementById(id);
+    const headerOffset = 72;
+    const elementPosition = element?.getBoundingClientRect().top ?? 0;
+    const offsetPosition = elementPosition + window.scrollY - headerOffset;
+
+    window?.scrollTo({
+      behavior: 'smooth',
+      top: offsetPosition,
+     });
+  }
+
   return (
-    <div className='flex py-5 justify-center sticky top-0 z-50 bg-white shadow-sm'>
-      <div className='flex p-3 md:p-0 md:flex-row justify-between max-w-screen-lg flex-1'>
+    <div className='flex py-3 justify-center sticky top-0 z-50 bg-white shadow-sm'>
+      <div className='flex p-3 md:flex-row justify-between max-w-screen-lg flex-1'>
         <div className='flex items-center gap-3'>
           <img src={PlaningIcon} alt="" width={20} />
           <p className='font-semibold'>Strategic planing</p>
@@ -27,10 +39,25 @@ export const Header = () => {
         </div>
 
         <div className={(isOpen ? 'flex absolute top-24 right-3 bg-white p-10 border-slate-300 border  rounded shadow-lg' : 'hidden') + ' sm:flex flex-col sm:flex-row gap-3'}>
-          <a className='hover:text-blue-600' href="#introduction">Introduction</a>
-          <a className='hover:text-blue-600' href="#diagnostic">Diagnostic</a>
-          <a className='hover:text-blue-600' href="#identity">Identity</a>
-          <a className='hover:text-blue-600' href="#objectives">Objectives</a>
+          <button
+            className='hover:text-blue-600'
+            onClick={() => scrollTo("introduction")}
+          >Introduction</button>
+
+          <button
+            className='hover:text-blue-600'
+            onClick={() => scrollTo("diagnostic")
+          }>Diagnostic</button>
+          
+          <button
+            className='hover:text-blue-600'
+            onClick={() => scrollTo("identity")}
+          >Identity</button>
+          
+          <button
+            className='hover:text-blue-600'
+            onClick={() => scrollTo("objectives")}
+          >Objectives</button>
         </div>
       </div>
     </div>
