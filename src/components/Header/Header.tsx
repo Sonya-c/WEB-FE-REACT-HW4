5dd30207fb5a1b4dc9e301cd1eaeb5694f20ed84
Sonya Castro <sonya.castro.gomez@gmail.com>
2024-09-16 00:27:29 -0500

    feat(Header): return to top

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,63 +1,86 @@
-import PlaningIcon from '@/assets/icons/planing.svg';
-import MenuIcon from '@/assets/icons/menu.svg';
-import { useState } from 'react';
+import PlaningIcon from "@/assets/icons/planing.svg";
+import MenuIcon from "@/assets/icons/menu.svg";
+import { useState } from "react";
 
 /**
- * This is a Header component. 
+ * This is a Header component.
  */
 export const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
-  
-  const scrollTo = (id: string) => {
-    const element = document.getElementById(id);
+
+  const scrollTo = (id?: string) => {
     const headerOffset = 72;
+
+    if (!id) {
+      window?.scrollTo({
+        behavior: "smooth",
+        top: headerOffset,
+      });
+      return;
+    }
+
+    const element = document.getElementById(id);
     const elementPosition = element?.getBoundingClientRect().top ?? 0;
     const offsetPosition = elementPosition + window.scrollY - headerOffset;
 
     window?.scrollTo({
-      behavior: 'smooth',
+      behavior: "smooth",
       top: offsetPosition,
-     });
-  }
+    });
+  };
 
   return (
-    <div className='flex py-3 justify-center sticky top-0 z-50 bg-white shadow-sm'>
-      <div className='flex p-3 md:flex-row justify-between max-w-screen-lg flex-1'>
-        <div className='flex items-center gap-3'>
+    <div className="sticky top-0 z-50 flex justify-center bg-white py-3 shadow-sm">
+      <div className="flex max-w-screen-lg flex-1 justify-between p-3 md:flex-row">
+        <button className="flex items-center gap-3" onClick={() => scrollTo()}>
           <img src={PlaningIcon} alt="" width={20} />
-          <p className='font-semibold'>Strategic planing</p>
-        </div>
+          <p className="font-semibold">Strategic planing</p>
+        </button>
 
-        <div className="sm:hidden flex">
+        <div className="flex sm:hidden">
           <button
-            aria-label='open menu'
-            className={isOpen ? 'rotate-90' : ''}
+            aria-label="open menu"
+            className={isOpen ? "rotate-90" : ""}
             onClick={() => setIsOpen(!isOpen)}
           >
             <img src={MenuIcon} alt="Menu" width={20} />
           </button>
         </div>
 
-        <div className={(isOpen ? 'flex absolute top-24 right-3 bg-white p-10 border-slate-300 border  rounded shadow-lg' : 'hidden') + ' sm:flex flex-col sm:flex-row gap-3'}>
+        <div
+          className={
+            (isOpen
+              ? "absolute right-3 top-24 flex rounded border border-slate-300 bg-white p-10 shadow-lg"
+              : "hidden") + " flex-col gap-3 sm:flex sm:flex-row"
+          }
+        >
           <button
-            className='hover:text-blue-600'
+            className="hover:text-blue-600"
             onClick={() => scrollTo("introduction")}
-          >Introduction</button>
+          >
+            Introduction
+          </button>
 
           <button
-            className='hover:text-blue-600'
-            onClick={() => scrollTo("diagnostic")
-          }>Diagnostic</button>
-          
+            className="hover:text-blue-600"
+            onClick={() => scrollTo("diagnostic")}
+          >
+            Diagnostic
+          </button>
+
           <button
-            className='hover:text-blue-600'
+            className="hover:text-blue-600"
             onClick={() => scrollTo("identity")}
-          >Identity</button>
-          
+          >
+            Identity
+          </button>
+
           <button
-            className='hover:text-blue-600'
+            className="hover:text-blue-600"
             onClick={() => scrollTo("objectives")}
-          >Objectives</button>
+          >
+            Objectives
+          </button>
         </div>
       </div>
     </div>
