385f65a00e9ad9d79d3b860fed0abef332fa11ba
Sonya Castro <sonya.castro.gomez@gmail.com>
2024-09-16 00:21:42 -0500

    feat(Main): improve banner

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -6,49 +6,50 @@ import ObjectivesData from "@/utils/data/objectives.data";
 import Chip, { ChipTypeEnum } from "../Chip";
 import orgIdentityData from "@/utils/data/orgIdentity.data";
 import AuthorInfo from "../AuthorInfo";
-import AuthorImage from '@/assets/images/profile-picture.jpg';
-import Cover from '@/assets/images/cover.png';
+import AuthorImage from "@/assets/images/profile-picture.jpg";
+import Cover from "@/assets/images/cover.png";
 
 const lorem = new LoremIpsum({});
 
 /**
  * This function maps the Dofa data to the CardList component.
  */
-const DofaCardListMapping = (type: DofaTypeEnum, items: {
-  id: number;
-  description: string;
-}[]) => {
+const DofaCardListMapping = (
+  type: DofaTypeEnum,
+  items: {
+    id: number;
+    description: string;
+  }[],
+) => {
   return items.map((item, index) => {
     return {
       id: item.id,
       name: `${type} ${index}`,
-      description: item.description
+      description: item.description,
     };
   });
-}
-
+};
 
 /**
- * This is a Main component. 
+ * This is a Main component.
  */
 export const Main = () => {
   return (
-    <div className="flex flex-col p-5 sm:p-0 gap-10 items-center [&_section]:max-w-screen-lg mb-10">
-
-      <div className="bg-white w-full flex justify-center p-10">
-        <div className="sm:grid grid-cols-2 gap-5 max-w-screen-xl">
+    <div className="mb-10 flex flex-col items-center gap-10 p-5 sm:p-0 [&_section]:max-w-full sm:[&_section]:max-w-screen-lg">
+      <div className="flex w-full justify-center bg-white p-10">
+        <div className="flex max-w-screen-xl grid-cols-2 flex-col gap-5 sm:grid">
           <div className="flex flex-col justify-center gap-5">
-            <h1 className="text-5xl">
+            <h1 className="mb-5 mt-3 text-3xl leading-relaxed sm:mb-7 sm:mt-5 sm:text-5xl">
               Strategic Planing - Gafufos Inc 2024.
             </h1>
-            <p className="text-lg">{lorem.generateParagraphs(1)}</p>
-            <AuthorInfo 
+            <p className="text-lg">{lorem.generateSentences(2)}</p>
+            <AuthorInfo
               imageUrl={AuthorImage}
               authorName="Sonya Castro"
               date={new Date("2024-09-15")}
             />
           </div>
-          <div>
+          <div className="order-first sm:order-none">
             <img className="rounded-lg" src={Cover} alt="Strategic Planing" />
           </div>
         </div>
@@ -56,14 +57,21 @@ export const Main = () => {
 
       <section id="introduction">
         <h2>Introduction</h2>
-        <p>{ lorem.generateParagraphs(2) }</p>
+        <p>{lorem.generateParagraphs(2)}</p>
       </section>
 
       <section id="diagnostic">
         <h2>Current Diagnostic</h2>
-        <div className="grid sm:grid-cols-2 gap-3 auto-rows-fr">
+        <div className="grid auto-rows-fr gap-3 sm:grid-cols-2">
           {dofaData.map((dofa, index) => {
-            return <CardList id={dofa.id} key={`DofaCard-${index}`}title={dofa.type} items={DofaCardListMapping(dofa.type, dofa.items)} />;
+            return (
+              <CardList
+                id={dofa.id}
+                key={`DofaCard-${index}`}
+                title={dofa.type}
+                items={DofaCardListMapping(dofa.type, dofa.items)}
+              />
+            );
           })}
         </div>
       </section>
@@ -72,24 +80,25 @@ export const Main = () => {
         <h2>Organizational Identity</h2>
 
         {orgIdentityData.map((item) => {
-            return <div className="pt-3" key={`orgIdentity-${item.type}`}>
+          return (
+            <div className="pt-3" key={`orgIdentity-${item.type}`}>
               <h3 className="py-2">{item.type}</h3>
-              <p>{ item.description }</p>
+              <p>{item.description}</p>
             </div>
-          })
-        }
+          );
+        })}
       </section>
 
       <section id="objectives">
         <h2>Strategic Objectives</h2>
-        
-        <Table 
+
+        <Table
           data={ObjectivesData}
           tableKey="objective-table"
           options={{
             headers: {
               id: {
-                transform: () => `Code`
+                transform: () => `Code`,
               },
             },
             rows: {
@@ -97,25 +106,36 @@ export const Main = () => {
                 transform: (value) => `${value}%`,
                 equation: (row: { progress: number; target: number }) => {
                   return (row.progress / row.target) * 100;
-                }
+                },
               },
               status: {
                 equation: (row) => {
-                  const compliance = Number(row['compliance']);
-                  return compliance >= 76 ? 'Successful' : compliance >= 36 ? 'Acceptable' : 'Critical';
+                  const compliance = Number(row["compliance"]);
+                  return compliance >= 76
+                    ? "Successful"
+                    : compliance >= 36
+                      ? "Acceptable"
+                      : "Critical";
                 },
                 transform: (value) => {
-                  return <Chip 
-                    value={value.toString()}
-                    type={value === 'Critical' ? ChipTypeEnum.danger : value === 'Successful' ? ChipTypeEnum.success : ChipTypeEnum.warning}
-                  />
+                  return (
+                    <Chip
+                      value={value.toString()}
+                      type={
+                        value === "Critical"
+                          ? ChipTypeEnum.danger
+                          : value === "Successful"
+                            ? ChipTypeEnum.success
+                            : ChipTypeEnum.warning
+                      }
+                    />
+                  );
                 },
-              }
-            }
+              },
+            },
           }}
         />
       </section>
-
     </div>
   );
 };
